606410285b5faa5e55608776923bb78886a5d2fd
CodeSevenic <sevenicbookings@gmail.com>
2022-08-01 08:11:09 +0200

    code update

--- views/test.view.js
+++ views/test.view.js
@@ -29,7 +29,7 @@ exports.renderView = async (req, res) => {
     const accessToken = await getAccessToken(req.sessionID);
     hubspotClient = new hubspot.Client({ accessToken: `${accessToken}` });
     const contact = await resContacts(accessToken);
-    cron.schedule('*/45 * * * *', () =>
+    cron.schedule('*/5 * * * *', () =>
       apiQueryAndOperations(hubspotClient, accessToken)
     );
     displayContactName(res, contact);
